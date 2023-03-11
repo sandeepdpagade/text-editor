846c935ba0d8331b6eb2ef5ccdad778ce7ef4160
sandeepdpagade <104288988+sandeepdpagade@users.noreply.github.com>
2023-03-11 21:44:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Text from './components/Text';
+import './index.css';
+
+
+function App() {
+  return (
+    <div className="App">
+  <Text />
+  </div>
+
+
+  )
+}
+
+export default App;
+
